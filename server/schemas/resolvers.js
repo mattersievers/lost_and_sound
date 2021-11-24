91463e389c45c02c6aae0d8fdcb13385e78621c8
Matthew Sievers <mattersievers@gmail.com>
2021-11-24 15:38:30 -0600

    added some  basic structure to typedefs and resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,15 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Equipment } = require('../models');
+const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+const resolvers = {
+    Query: {
+
+    },
+    Mutation: {
+
+    }
+}
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,19 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Equipment {
+        _id: ID
+    }
+
+    type User {
+        _id: ID
+    }
+
+    type Query {
+
+    }
+
+    type Mutation {
+        
+    }
+`;    

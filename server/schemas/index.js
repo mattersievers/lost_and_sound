12e4fc84eefd78e9a3fe0262b897d7e72f7090c6
LangdonEcho <86031485+LangdonEcho@users.noreply.github.com>
2021-11-24 20:21:56 -0600

    adding the models

---
+++ server/schemas/index.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const ItemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  location: { type: String, required: true },
+  lnglat: { type: String, required: true },
+  type: { type: String, required: true },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  comment: String,
+  time: String,
+  active: Boolean,
+  category: String,
+  tags: Array,
+  imageUrl: String,
+  date: Date
+});
+
+ItemSchema.pre('save', function(next) {
+  this.date = new Date();
+  next();
+});
+
+module.exports = mongoose.model('Item', ItemSchema);

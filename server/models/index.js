f9fa0aeba046b0a95be1f0eebbb3e6b56df24d2e
Matthew Sievers <mattersievers@gmail.com>
2021-11-27 16:02:14 -0600

    graphql functions all work

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,3 @@
 const User = require('./usermodel');
 
-
 module.exports = { User };
--- server/models/itemmodel.js
+++ server/models/itemmodel.js
@@ -1,19 +1,17 @@
 const mongoose = require('mongoose');
 
 const ItemSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  location: { type: String, required: true },
-  lnglat: { type: String, required: true },
+  category: { type: String, required: true },
+  brand: { type: String, required: true },
+  model: { type: String, required: true },
+  description: { type: String, required: true },
+  serialNumber: { type: String, required: true },
+  image: { type: String, required: true },
   type: { type: String, required: true },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
+  location: { type: String, required: true },
+  lost: {type: Boolean, required: true },
   comment: String,
-  time: String,
-  active: Boolean,
-  category: String,
+  lostTime: String,
   date: Date
 });
 

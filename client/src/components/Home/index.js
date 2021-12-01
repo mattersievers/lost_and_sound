587345cc65666fc1320eaa73f2c5ad09b4f544b6
Darrik Crawford <darrikcrawford@yahoo.com>
2021-11-30 23:31:01 -0600

    display equipment on home page

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,8 +1,15 @@
+import { useQuery } from '@apollo/client';
 import React from 'react';
 import { GET_EQUIPMENT } from '../../utils/queries';
 
 
-const Home = () => {
+function Home() {
+    const { loading, data: userData } = useQuery(GET_EQUIPMENT);
+    
+    if(loading) {
+        return <h1> LOADING... </h1>
+    }
+
     return(
         <section className="home d-flex flex-column ">
             <div className="align-self-center">
@@ -13,17 +20,35 @@ const Home = () => {
             </div>
             
             <div>
-                
-                <h1 className="equipCat" {users.savedEquipment.category}></h1>
-                <p className="equipBrand">{users.equipment.brand}</p>
-                <p className="equipModel">{users.equipment.model}</p>
-                <p className="equipDesc">{users.equipment.description}</p>
-                <p className="equipNumber">{users.equipment.serialNumber}</p>
-                <p className="equipImage">{users.equipment.image}</p>
-                <p className="equipLocation">{users.equipment.location}</p>
-                <p className="equipModel">{users.equipment.model}</p>
-                
+                <h1>Lost Equipment</h1>
             </div>
+            
+            <div>
+                {userData.users.map((user) => {
+                    return (
+                    <>
+                    <div>
+                    {user.firstName} {user.lastName}
+                    </div>
+                    {user.savedEquipment.map((item) => {
+                        if (item.lost) {
+                    return(
+                        <ul>
+                            <li>Category: `{item.category}`</li>
+                            <li>Brand: `{item.brand}`</li>
+                            <li>Model: `{item.model}`</li>
+                            <li>Description: `{item.description}`</li>
+                            <li>Serial Number:  `{item.serialNumber}`</li>
+                            <li>Image: `{item.image}`</li>
+                            
+                        </ul>
+                    )}
+                    })};
+                    </>
+                    )
+                })}
+            </div>
+        
           
         </section>
     )
